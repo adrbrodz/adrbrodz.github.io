48441aa291f20c92b15102e724f61ee2f168dd28
Adrian <adrbrodz@gmail.com>
2025-09-16 12:16:53 +0200

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Adrian Brodziński — Frontend Developer",
+  title: "Adrian Brodziński - Frontend Developer",
   description: "",
 };
 
